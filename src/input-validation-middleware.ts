f41fc4dc69ce9b01afc48435bb3db483fa5c2299
German Goldin <german.goldin@bankleumi.co.il>
2022-10-29 22:47:22 +0300

    commit 38

--- src/input-validation-middleware.ts
+++ src/input-validation-middleware.ts
@@ -5,6 +5,9 @@ export const inputValidationMiddleware = (req: Request, res: Response, next: Nex
     if(req.headers.authorization !== 'Basic YWRtaW46cXdlcnR5') res.status(401).send()
     const errors = validationResult(req)
 if(!errors.isEmpty()) {
+    const newErrors = errors.array().map(err => {return {"message": err.msg, "field": err.param}})
+    const nameError = newErrors.find(err => err.field === 'name')
+    const youtubeUrlError = newErrors.find(err => err.field === 'youtubeUrl')
   return res.status(400).json({
     "errorsMessages": errors.array().map(err => {return {"message": err.msg, "field": err.param}})
   })
